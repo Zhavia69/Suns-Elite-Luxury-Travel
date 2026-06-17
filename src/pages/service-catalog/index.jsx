890f9caa9230fb24388be995d6e39e3e1ea29c07
Zhavia69 <sammuelryan4050@gmail.com>
2026-06-17 22:28:55 +0300

    h1

--- src/pages/service-catalog/index.jsx
+++ src/pages/service-catalog/index.jsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 import { motion, useScroll, useSpring } from "framer-motion";
 import { ArrowRight, Check } from "lucide-react";
 import Header from "../../components/ui/Header";
-import LuxuryFooter from "../homepage/index";
+import LuxuryFooter from "../homepage";
 
 const CATALOG_SERVICES = [
   {
